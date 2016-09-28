1a367e4eb21da9c59543c85224670a76c6f70863
Veskor <yugio33@yahoo.com>
2016-09-29 01:15:43 +0200

    update!!!

--- MicroControlerScripts/script1.py
+++ MicroControlerScripts/script1.py
@@ -3,8 +3,8 @@ import paramiko
 def uploadCode(ip, username, password, filename):
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(ip,port=8000,username=username,password=password)
+        ssh.connect(ip,port=22,username=username,password=password)
         ftp = ssh.open_sftp()
-        #ftp.put(filename, 'remotefile.py')
-        stdin, stdout, stderr = ssh.exec_command("touch remotefile")
+        ftp.put(filename, 'remotefile.py')
+        stdin, stdout, stderr = ssh.exec_command("python remotefile")
         ftp.close()
--- MicroControlerScripts/upload.sh
+++
@@ -1,4 +0,0 @@
-host=$1
-password=$2
-code=$3
-sshpass -p "$password" ssh $host "echo $code > output.py"
